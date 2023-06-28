51f29541d829070d8480384253ce0a8eacd0cb29
Charon-Fan <Charon.Dian@gmail.com>
2023-06-28 15:12:00 +0800

    fix: adjust display issues with Twitter & YouTube

--- docu-notion.config.ts
+++ docu-notion.config.ts
@@ -6,8 +6,25 @@ export const youtubeEmbed: IPlugin = {
     {
       regex: /\[image\]\((.*youtube\.com\/watch.*)\)/,
       imports: [`import ReactPlayer from "react-player";`],
-      replacementPattern: `<ReactPlayer controls url="$1" />`,
-    },
+      replacementPattern: `
+<div
+  style={{
+    position: 'relative',
+    paddingTop: '56.25%',
+  }}
+>
+  <ReactPlayer
+    url="$1"
+    width="100%"
+    height="100%"
+    style={{
+      position: 'absolute',
+      top: 0,
+      left: 0,
+    }}
+  />
+</div>`,
+},
   ],
 };
 
@@ -17,7 +34,21 @@ export const twitterEmbed: IPlugin = {
     {
       regex: /\[embed\]\(https?:\/\/twitter\.com\/(?:\w+)\/status\/(\d+)(?:\?(?:[\w-]+=[\w-]+&)*(?:[\w-]+=[\w-]+))?\)/,
       imports: [`import { TwitterTweetEmbed } from "react-twitter-embed";`],
-      replacementPattern: `<TwitterTweetEmbed tweetId="$1" />`,
+      replacementPattern: `
+<div
+  style={{
+    display: 'flex',
+    justifyContent: 'center',
+  }}
+>
+  <div style={{
+    maxWidth: '420px',
+    width: '100%',
+  }}>
+    <TwitterTweetEmbed tweetId="$1" />
+  </div>
+</div>
+`,
     },
   ],
 }
