7490d7cec4be09469fb4e1247b141f7f3ac8b041
Charon-Fan <Charon.Dian@gmail.com>
2023-08-09 11:27:00 +0800

    Revert "feat: remove navbar & footer"
    This reverts commit 18d1a6fd423e5b932fd98168aa5bd6148f386f98.

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -93,78 +93,78 @@ const config = {
     ({
       // Replace with your project's social card
       image: 'img/docusaurus-social-card.jpg',
-      // navbar: {
-      //   title: 'Keystone',
-      //   logo: {
-      //     alt: 'Keystone Logo',
-      //     src: 'img/logo.svg',
-      //     srcDark: 'img/logo_dark.svg',
-      //   },
-      //   hideOnScroll: false,
-      //   items: [
-      //     {
-      //       type: 'docSidebar',
-      //       sidebarId: 'tutorialSidebar',
-      //       position: 'left',
-      //       label: 'Tutorial',
-      //     },
-      //     { to: '/blog', label: 'Blog', position: 'left' },
-      //     {
-      //       type: 'localeDropdown',
-      //       position: 'right',
-      //     },
-      //     {
-      //       href: 'https://github.com/facebook/docusaurus',
-      //       label: 'GitHub',
-      //       position: 'right',
-      //     },
-      //   ],
-      // },
-      // footer: {
-      //   style: 'dark',
-      //   links: [
-      //     {
-      //       title: 'Docs',
-      //       items: [
-      //         {
-      //           label: 'Tutorial',
-      //           to: '/docs',
-      //         },
-      //       ],
-      //     },
-      //     {
-      //       title: 'Community',
-      //       items: [
-      //         {
-      //           label: 'Stack Overflow',
-      //           href: 'https://stackoverflow.com/questions/tagged/docusaurus',
-      //         },
-      //         {
-      //           label: 'Discord',
-      //           href: 'https://discordapp.com/invite/docusaurus',
-      //         },
-      //         {
-      //           label: 'Twitter',
-      //           href: 'https://twitter.com/docusaurus',
-      //         },
-      //       ],
-      //     },
-      //     {
-      //       title: 'More',
-      //       items: [
-      //         {
-      //           label: 'Blog',
-      //           to: '/blog',
-      //         },
-      //         {
-      //           label: 'GitHub',
-      //           href: 'https://github.com/facebook/docusaurus',
-      //         },
-      //       ],
-      //     },
-      //   ],
-      //   copyright: `Keystone © All rights reserved 2018 - ${new Date().getFullYear()}`
-      // },
+      navbar: {
+        title: 'Keystone',
+        logo: {
+          alt: 'Keystone Logo',
+          src: 'img/logo.svg',
+          srcDark: 'img/logo_dark.svg',
+        },
+        hideOnScroll: false,
+        items: [
+          {
+            type: 'docSidebar',
+            sidebarId: 'tutorialSidebar',
+            position: 'left',
+            label: 'Tutorial',
+          },
+          { to: '/blog', label: 'Blog', position: 'left' },
+          {
+            type: 'localeDropdown',
+            position: 'right',
+          },
+          {
+            href: 'https://github.com/facebook/docusaurus',
+            label: 'GitHub',
+            position: 'right',
+          },
+        ],
+      },
+      footer: {
+        style: 'dark',
+        links: [
+          {
+            title: 'Docs',
+            items: [
+              {
+                label: 'Tutorial',
+                to: '/docs',
+              },
+            ],
+          },
+          {
+            title: 'Community',
+            items: [
+              {
+                label: 'Stack Overflow',
+                href: 'https://stackoverflow.com/questions/tagged/docusaurus',
+              },
+              {
+                label: 'Discord',
+                href: 'https://discordapp.com/invite/docusaurus',
+              },
+              {
+                label: 'Twitter',
+                href: 'https://twitter.com/docusaurus',
+              },
+            ],
+          },
+          {
+            title: 'More',
+            items: [
+              {
+                label: 'Blog',
+                to: '/blog',
+              },
+              {
+                label: 'GitHub',
+                href: 'https://github.com/facebook/docusaurus',
+              },
+            ],
+          },
+        ],
+        copyright: `Keystone © All rights reserved 2018 - ${new Date().getFullYear()}`
+      },
       prism: {
         theme: lightCodeTheme,
         darkTheme: darkCodeTheme,
