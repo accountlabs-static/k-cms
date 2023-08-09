59dd579fb258d4b92c71ca4bd2fb6c72633e3d65
Charon-Fan <Charon.Dian@gmail.com>
2023-08-09 11:51:40 +0800

    docs: adjust copy

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,7 +6,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: 'Keystone',
-  tagline: 'Dinosaurs are cool',
+  tagline: 'Keystone are cool',
   favicon: 'img/favicon.svg',
 
   // Set the production url of your site here
