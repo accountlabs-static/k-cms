962dfc85f0892777c8524a6b3092361c76c3a9c3
Charon-Fan <Charon.Dian@gmail.com>
2023-08-09 11:59:50 +0800

    chore: adjust config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -11,24 +11,14 @@ const config = {
 
   // Set the production url of your site here
   url: 'https://k-cms-lemon.vercel.app/',
-  // Set the /<baseUrl>/ pathname under which your site is served
-  // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
-
-  // GitHub pages deployment config.
-  // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'Keystone support', // Usually your repo name.
-
+  organizationName: 'KeystoneHQ',
+  projectName: 'Keystone support',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
-
-  // Even if you don't use internalization, you can use this field to set useful
-  // metadata like html lang. For example, if your site is Chinese, you may want
-  // to replace "en" with "zh-Hans".
   i18n: {
     defaultLocale: 'en',
-    locales: ['en', 'zh-Hant', 'ko'],
+    locales: ['en', 'zh-Hant'], // ['en', 'zh-Hant', 'ko'],
   },
   plugins: [
     [
@@ -59,15 +49,9 @@ const config = {
           sidebarPath: require.resolve('./sidebars.js'),
           remarkPlugins: [math],
           rehypePlugins: [katex],
-          // editUrl:
-          //   'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
         },
         blog: {
           showReadingTime: true,
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          // editUrl:
-          //   'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
@@ -91,10 +75,8 @@ const config = {
   themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
-      // Replace with your project's social card
       image: 'img/social-card.jpg',
       navbar: {
-        // title: 'Keystone',
         logo: {
           alt: 'Keystone Logo',
           src: 'img/logo.svg',
