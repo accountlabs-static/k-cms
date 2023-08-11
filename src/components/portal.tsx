85e33a93e97ef8c162a5368bd9ac23526ba3bd95
Charon-Fan <Charon.Dian@gmail.com>
2023-08-11 15:31:02 +0800

    chore: change default route

--- src/components/portal.tsx
+++ src/components/portal.tsx
@@ -3,7 +3,7 @@ import { Redirect } from '@docusaurus/router';
 import useBaseUrl from '@docusaurus/useBaseUrl';
 
 function Portal() {
-  const url = useBaseUrl('/docs/get-started');
+  const url = useBaseUrl('/docs/Keystone-Gen-3/');
   return <Redirect to={url} />;
 }
 
