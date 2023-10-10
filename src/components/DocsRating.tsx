dc43cc89ca612155c426e42ccf6f39b6b776b456
rea <hi@rea.ink>
2023-10-10 11:50:41 +0800

    fix: use enum

--- src/components/DocsRating.tsx
+++ src/components/DocsRating.tsx
@@ -17,32 +17,44 @@ import HappyIcon from '@site/static/img/ratings/happy.svg';
 import MediumIcon from '@site/static/img/ratings/medium.svg';
 import BadIcon from '@site/static/img/ratings/bad.svg';
 
+enum Feedback {
+  Positive = 'positive',
+  Neutral = 'neutral',
+  Negative = 'negative',
+}
+
+enum FeedbackNegativeReason {
+  Outdated = 'outdated',
+  CannotProceed = 'cannotProceed',
+  Other = 'other',
+}
+
 const DocsRating = ({ label }) => {
   if (!ExecutionEnvironment.canUseDOM) {
     return null;
   }
   const { metadata } = useDoc();
 
-  const [voteValue, setVoteValue] = useState(null);
-  const giveFeedback = (value: string) => {
+  const [voteValue, setVoteValue] = useState<Feedback | FeedbackNegativeReason>(null);
+  const giveFeedback = (feedback: Feedback | FeedbackNegativeReason) => {
     const day = dayjs()
     const docRef = doc(firestore, 'docs', metadata.title);
     setDoc(docRef, {}, { merge: true }).then(() => {
       const docRatingRef = doc(firestore, "docs", metadata.title);
       updateDoc(docRatingRef, {
         path: metadata.permalink,
-        [`all.${value}`]: increment(1),
-        [`dates.${day.format('YYYY')}.${day.format('MM')}.${value}`]: increment(1),
+        [`all.${feedback}`]: increment(1),
+        [`dates.${day.format('YYYY')}.${day.format('MM')}.${feedback}`]: increment(1),
         editTime: new Date(),
       });
     });
 
-    setVoteValue(value);
+    setVoteValue(feedback);
   };
 
   const formHref = useMemo(() => `https://docs.google.com/forms/d/e/1FAIpQLScs4qDvah9XsxYEEcnELQmn10uU52SgkFVvr4TBmrsPJZKS0g/viewform?usp=pp_url&entry.1011131940=${metadata.title + ' | ' + metadata.permalink}`, [metadata.title])
 
-  if (voteValue === 'bad') {
+  if (voteValue === Feedback.Negative) {
     return <div className="docsRating bad-reasons">
       <span className="text-right">
         {translate({
@@ -50,14 +62,14 @@ const DocsRating = ({ label }) => {
         })}
       </span>
       <div className="bad-selections">
-        <div className="select-box" onClick={() => giveFeedback('outdated')}>
+        <div className="select-box" onClick={() => giveFeedback(FeedbackNegativeReason.Outdated)}>
           {translate({ id: 'docs.footer.ratings.bad.selection.outdated' })}
         </div>
-        <div className="select-box" onClick={() => giveFeedback('Cannot proceed according')}>
+        <div className="select-box" onClick={() => giveFeedback(FeedbackNegativeReason.CannotProceed)}>
           {translate({ id: 'docs.footer.ratings.bad.selection.cannotAccording' })}
         </div>
         <a href={formHref} target="_blank">
-          <div className="select-box" onClick={() => setVoteValue('other')}>
+          <div className="select-box" onClick={() => setVoteValue(FeedbackNegativeReason.Other)}>
             <EditIcon />
             {translate({ id: 'docs.footer.ratings.bad.selection.other' })}
           </div>
@@ -74,9 +86,9 @@ const DocsRating = ({ label }) => {
         <>
           <span>{translate({ id: 'docs.footer.ratings.title' })}</span>
           <div className="vote-box">
-            <HappyIcon onClick={() => giveFeedback('like')} />
-            <MediumIcon onClick={() => giveFeedback('medium')} />
-            <BadIcon onClick={() => giveFeedback('bad')} />
+            <HappyIcon onClick={() => giveFeedback(Feedback.Positive)} />
+            <MediumIcon onClick={() => giveFeedback(Feedback.Neutral)} />
+            <BadIcon onClick={() => giveFeedback(Feedback.Negative)} />
           </div>
         </>
       )}
